b016dc524f12711bd24a3b5a05f6397e17eb7e96
Nick Scratch <drevilbones@gmail.com>
2024-12-03 22:12:41 -0500

    numpad, mouse button hold, LED configs, other stuff

--- keymap.c
+++ keymap.c
@@ -16,13 +16,11 @@
   */
   // SOFLE RGB
 #include <stdio.h>
-
+#include "keymap_introspection.h"
 #include QMK_KEYBOARD_H
 
 enum my_keycodes {
-  MB1HLD = SAFE_RANGE,
-  MB2HLD,
-  MB3HLD
+  MB1HLD = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT( //function
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,  
   _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, KC_F12,
-  _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, TG(5),
   KC_CAPS, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
                  _______, _______, _______, KC_MPLY, KC_MPRV,      KC_MNXT, KC_MPLY, _______, _______, _______
 ),
@@ -61,18 +59,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [4] = LAYOUT( //mouse
   _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                  _______, MB1HLD,  _______, _______,  _______, _______,
+  _______, _______, _______, _______, _______, _______,                  _______, MB1HLD,  _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______,                  _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______,
                  _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
 ),
 
 [5] = LAYOUT( //Numpad
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_NUM,  XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_NUM,  XXXXXXX, XXXXXXX,XXXXXXX, _______,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_P7,  KC_P8,   KC_P9,   KC_ASTR, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   KC_EQL,  KC_PIPE,
+  TG(5),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   KC_EQL,  KC_PIPE,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,   _______,KC_PLUS, KC_P1,  KC_P2,   KC_P3,   KC_SLSH, _______,
-              _______, _______, _______, _______, _______,          _______, _______,  KC_P0,   KC_PDOT, _______
+              _______, _______, _______, _______, _______,      _______, _______,  KC_P0,   KC_PDOT, _______
 ),
 
 /*
@@ -87,26 +85,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+// TODO: maybe make this output held status to oled?
+// MB1HLD: holds mouse button 1 until pushed again, useful for dragging and highlighting
 bool mb1held = false;
-//bool mb2held = false;
-//bool mb3held = false;
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case MB1HLD:
       if (record->event.pressed) {
-        if (!mb1held) {
-          SEND_STRING(SS_DOWN(MS_BTN1));
-          mb1held = true;
-        } else {
-          SEND_STRING(SS_UP(MS_BTN1));
+        if (mb1held) {
+          SEND_STRING(SS_UP(X_BTN1));
           mb1held = false;
+        } else {
+          SEND_STRING(SS_DOWN(X_BTN1));
+          mb1held = true;
         }
       }
       return false;
     default:
       return true;
   }
+}
+
+// register our ms button 1 hold key as an official "mouse key"
+bool is_mouse_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch(keycode) {
+        case MB1HLD:
+            return true;
+        default:
+            return false;
+    }
+    return false;
 }
 
 
@@ -126,10 +134,24 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 
 // index 0 and 36: indicator LEDs
-bool rgb_matrix_indicators_user() {
-  switch(get_highest_layer(layer_state)) {
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+  uint8_t layer = get_highest_layer(layer_state);
+  switch(layer) {
     case 4:
       rgb_matrix_set_color(0, RGB_CYAN);
+      break;
+    case 5: //numlock color coding
+      for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+          for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+              uint8_t index = g_led_config.matrix_co[row][col];
+
+              uint16_t kcatkml = keycode_at_keymap_location(layer, row, col);
+              if (index >= led_min && index < led_max && index != NO_LED) {
+                if (kcatkml == XXXXXXX) { rgb_matrix_set_color(index, RGB_OFF); } // if it's off, then LED is off
+                else { rgb_matrix_set_color(index, RGB_RED); } // else it's a numpad key so it's red
+              }
+          }
+      }
       break;
     default:
       rgb_matrix_set_color(0, RGB_OFF);
@@ -141,6 +163,8 @@ bool rgb_matrix_indicators_user() {
 
  
 #ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
+
 void print_status(void) {
     oled_write_P(PSTR("\n\n"), false);
     oled_write_ln_P(PSTR("LAYER"), false);
@@ -158,7 +182,7 @@ void print_status(void) {
             oled_write_P(PSTR("LED\n"), false);
             break;
         case 4:
-            oled_write_P(PSTR("MOUSE\n"), false);
+            oled_write_P(PSTR("MOUSE"), false);
             break;
         case 5:
             oled_write_P(PSTR("NUM\n"), false);
@@ -166,6 +190,11 @@ void print_status(void) {
         default:
             oled_write_ln_P(PSTR("???\n"), false);
     }
+
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_ln(get_u8_str(rgb_matrix_config.hsv.h, '0'), false);
+    oled_write_ln(get_u8_str(rgb_matrix_config.hsv.s, '0'), false);
+    oled_write_ln(get_u8_str(rgb_matrix_config.hsv.v, '0'), false);
 }
 
 
